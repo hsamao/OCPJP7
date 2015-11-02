e88597118db3c09e93e86bc1f82c4c32543b42d3
hsamao <hsamao86@gmail.com>
2015-11-02 16:24:34 -0500

    listing 6.10 ==> Never Mix Raw Type and Generic Type

--- src/main/java/com/samao/ocpjp/chapter06/generics/and/collections/RawTest.java
+++ src/main/java/com/samao/ocpjp/chapter06/generics/and/collections/RawTest.java
@@ -9,23 +9,33 @@ import java.util.List;
  */
 public class RawTest {
 
-    public static void main (String [] args) {
+    public static void main(String[] args) throws Exception {
 
         List list = new LinkedList();
         list.add("first");
         list.add("second");
 
         List<String> stringList = list;
+//        stringList.add(10);  ==> results in erroneous at compilation time
 
-        for (Iterator<String> itemIterator = stringList.iterator(); itemIterator.hasNext();)
+        for (Iterator<String> itemIterator = stringList.iterator(); itemIterator.hasNext(); )
             System.out.println("Item : " + itemIterator.next());
 
         List<String> stringList1 = new LinkedList<>();
         stringList1.add("First");
         stringList1.add("Second");
 
-        List list1 = stringList1;
-        for (Iterator<String> itemIterator = list1.iterator(); itemIterator.hasNext();)
-            System.out.println("Item : " + itemIterator.next());
+
+        try {
+            List list1 = stringList1;
+            list1.add(10); // ==>> results in erroneous at runtime
+
+            for (Iterator<String> itemIterator = list1.iterator(); itemIterator.hasNext(); )
+                System.out.println("Item : " + itemIterator.next());
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+
+        }
     }
 }
