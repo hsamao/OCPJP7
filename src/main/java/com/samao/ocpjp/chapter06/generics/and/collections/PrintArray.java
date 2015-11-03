dd2bcc5c1458415a0005414332c05c606d998da0
hsamao <hsamao86@gmail.com>
2015-11-03 17:28:33 -0500

    listing 6.26 ==> Arrays Algorithm

---
+++ src/main/java/com/samao/ocpjp/chapter06/generics/and/collections/PrintArray.java
@@ -0,0 +1,15 @@
+package com.samao.ocpjp.chapter06.generics.and.collections;
+
+/**
+ * Created by hsamao on 11/3/15.
+ */
+public class PrintArray {
+    public static void main (String [] args) {
+
+        int [] intArray = {1, 2, 3, 4, 5};
+
+        System.out.println("The array contents are " + intArray);
+
+
+    }
+}
