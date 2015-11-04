9ffce75bff357115da3c066311df2bade1200d1b
hsamao <hsamao86@gmail.com>
2015-11-03 19:04:30 -0500

    listing 7.2 ==> indexOf method and using escape a character \

---
+++ src/main/java/com/samao/ocpjp/chapter07/processing/string/SearchString2.java
@@ -0,0 +1,14 @@
+package com.samao.ocpjp.chapter07.processing.string;
+
+/**
+ * Created by hsamao on 11/3/15.
+ */
+public class SearchString2 {
+
+    public static void main (String [] args){
+
+        String statement = "I am preparing for OCPJP";
+
+        System.out.println("Substring \"for\" occurs at index " + statement.indexOf("for"));
+    }
+}
