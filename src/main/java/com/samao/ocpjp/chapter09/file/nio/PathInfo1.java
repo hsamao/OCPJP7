5fc7235d2eaf54a0821af59b925f0028d0d0bc40
hsamao <hsamao86@gmail.com>
2015-11-05 18:21:51 -0500

    listing 9.1 ==> Getting Path Information

---
+++ src/main/java/com/samao/ocpjp/chapter09/file/nio/PathInfo1.java
@@ -0,0 +1,23 @@
+package com.samao.ocpjp.chapter09.file.nio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by hsamao on 11/5/15.
+ */
+public class PathInfo1 {
+
+    public static void main (String [] args){
+        Path path = Paths.get("/Users/hsamao/textfile.txt");
+
+        System.out.println("File path details\n");
+        System.out.println("\t File name : \t" + path.getFileName());
+        System.out.println("\t Root File : \t" + path.getRoot());
+        System.out.println("\t File parent : \t" + path.getParent());
+        System.out.println("\n**** File Elements ****\n");
+        for (Path path1 : path){
+            System.out.println("\t Path element : " + path1);
+        }
+    }
+}
