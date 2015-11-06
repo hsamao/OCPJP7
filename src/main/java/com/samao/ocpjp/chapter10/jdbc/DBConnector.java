8adb9266c4c8323814cdc10d1cb7e4d677a55d2a
hsamao <hsamao86@gmail.com>
2015-11-06 18:53:49 -0500

    10.2 ==> Database Connector Utility

--- src/main/java/com/samao/ocpjp/chapter10/jdbc/DBConnector.java
+++ src/main/java/com/samao/ocpjp/chapter10/jdbc/DBConnector.java
@@ -2,22 +2,34 @@ package com.samao.ocpjp.chapter10.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Arrays;
 
 /**
  * Created by hsamao on 11/6/15.
  */
 public class DBConnector {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
         String url = "jdbc:mysql://localhost:3307/";
         String database = "addressBook";
         String userName = "root";
         String password = "";
 
-        try (Connection connection = DriverManager.getConnection(url + database, userName, password)){
+        try (Connection connection = DriverManager.getConnection(url + database, userName, password)) {
 
             System.out.println("connected successfully to mysql database");
+
+            connection.close();
+            System.out.println("Database connection has been closed");
+
+            /*Statement statement = connection.createStatement();
+            String sql = "select * from contact;";
+            ResultSet resultSet = statement.executeQuery(sql);*/
+
+
 
         } catch (Exception ex) {
             System.out.println("Connection Failed");
---
+++ src/main/java/com/samao/ocpjp/chapter10/jdbc/DbConnectorUtility.java
@@ -0,0 +1,19 @@
+package com.samao.ocpjp.chapter10.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Created by hsamao on 11/6/15.
+ */
+public class DbConnectorUtility {
+
+    public Connection connectToDB () throws Exception {
+
+        String url = "jdbc:mysql://localhost:3307/";
+        String database = "addressBook";
+        String userName = "root";
+        String password = "";
+        return (DriverManager.getConnection(url+database, userName, password));
+    }
+}
