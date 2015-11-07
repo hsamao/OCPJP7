3d0370c942a2c4aceda3a7a173d6f14b9de1b73d
hsamao <hsamao86@gmail.com>
2015-11-07 12:55:56 -0500

    listin 10.12 ==> SavePoint and RollBack Operations

---
+++ src/main/java/com/samao/ocpjp/chapter10/jdbc/DbSavePoint.java
@@ -0,0 +1,66 @@
+package com.samao.ocpjp.chapter10.jdbc;
+
+import java.sql.*;
+
+/**
+ * Created by hsamao on 11/7/15.
+ */
+public class DbSavePoint {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DbConnectorUtility.connectToDB();
+            System.out.println("Connected to database successfully");
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+            resultSet = statement.executeQuery("SELECT * FROM FAMILY_GROUP;");
+            System.out.println("FAMILY_GROUP contents before updating ");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("NICKNAME"));
+            }
+
+            resultSet.moveToInsertRow();
+            resultSet.updateString("NICKNAME", "TOM");
+            resultSet.insertRow();
+            System.out.println("Inserted row for Tom");
+            Savepoint firstSavePoint = connection.setSavepoint();
+
+            resultSet.moveToInsertRow();
+            resultSet.updateString("NICKNAME", "Dick ");
+            resultSet.insertRow();
+            System.out.println("Inserted row for Dick");
+            Savepoint secondSavePoint = connection.setSavepoint();
+
+            resultSet.moveToInsertRow();
+            resultSet.updateString("NICKNAME", "Harry");
+            resultSet.insertRow();
+            Savepoint thirdSavePoint = connection.setSavepoint();
+            System.out.println("Inserted row for Harry");
+
+            connection.rollback(secondSavePoint);
+            System.out.println("Rolled back to previous last insertion");
+            // connection.commit();
+
+            System.out.println("FAMILY_GROUP AFTER UPDATING\n");
+
+            ResultSet resultSet1 = statement.executeQuery("SELECT * FROM FAMILY_GROUP;");
+            while (resultSet1.next()) {
+                System.out.println(resultSet1.getString("NICKNAME"));
+            }
+
+
+        } catch (SQLException ex) {
+            connection.rollback();
+            System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Failed");
+            System.out.println(ex.getMessage());
+        } finally {
+            if (connection != null) connection.close();
+            if (resultSet != null) resultSet.close();
+        }
+    }
+}
