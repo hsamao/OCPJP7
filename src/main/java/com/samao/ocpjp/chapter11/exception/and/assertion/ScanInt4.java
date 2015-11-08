204dae9c07e87339a7c89e8b9cb68f6d72ac826d
hsamao <hsamao86@gmail.com>
2015-11-08 11:49:42 -0500

    listing 11.5 ==> Multiple Catch Blocks

--- src/main/java/com/samao/ocpjp/chapter11/exception/and/assertion/ScanInt4.java
+++ src/main/java/com/samao/ocpjp/chapter11/exception/and/assertion/ScanInt4.java
@@ -1,14 +1,15 @@
 package com.samao.ocpjp.chapter11.exception.and.assertion;
 
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
  * Created by hsamao on 11/8/15.
  */
-public class ScanInt3 {
+public class ScanInt4 {
     public static void main (String [] args){
-        String integerStr = "100";
+        String integerStr = "";
         System.out.println("The string to scan integer from it is: " + integerStr);
         Scanner consoleScanner = new Scanner(integerStr);
 
@@ -16,6 +17,10 @@ public class ScanInt3 {
             System.out.println("The integer value scanned from string is: " + consoleScanner.nextInt());
         } catch (InputMismatchException ex){
             System.out.println("Error: Cannot scan an integer from the given string");
+        } catch (NoSuchElementException ex){
+            System.out.println("Error: Cannot scan an integer from the given string");
+        } catch (IllegalStateException ex){
+            System.out.println("Error: nextInt() called on a closed Scanner object");
         }
     }
 }
