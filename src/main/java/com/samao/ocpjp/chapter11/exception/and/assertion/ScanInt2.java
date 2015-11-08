3fdb4ab6748f3bc92afa6a3aa2a518459c25a08a
hsamao <hsamao86@gmail.com>
2015-11-08 11:17:52 -0500

    listing 11.3 ==> Try and Catch Statement

---
+++ src/main/java/com/samao/ocpjp/chapter11/exception/and/assertion/ScanInt2.java
@@ -0,0 +1,20 @@
+package com.samao.ocpjp.chapter11.exception.and.assertion;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by hsamao on 11/8/15.
+ */
+public class ScanInt2 {
+    public static void main(String[] args) {
+        System.out.println("Type an integer in the console : ");
+        Scanner consoleScanner = new Scanner(System.in);
+
+        try {
+            System.out.println("You typed the integer value: " + consoleScanner.nextInt());
+        } catch (InputMismatchException ex) {
+            System.out.println("Error: you typed some text that is not an integer value...");
+        }
+    }
+}
