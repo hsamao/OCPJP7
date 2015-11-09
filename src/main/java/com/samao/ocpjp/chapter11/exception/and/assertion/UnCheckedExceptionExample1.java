d1dcf8e1c202f2f1c76b2cf0476df53d107cc899
hsamao <hsamao86@gmail.com>
2015-11-08 22:26:54 -0500

    listing 11.15 => Handlding Unchecked Exception

--- src/main/java/com/samao/ocpjp/chapter11/exception/and/assertion/UnCheckedExceptionExample1.java
+++ src/main/java/com/samao/ocpjp/chapter11/exception/and/assertion/UnCheckedExceptionExample1.java
@@ -8,9 +8,12 @@ import java.io.FileNotFoundException;
  */
 public class UnCheckedExceptionExample1 {
 
-    public static void main(String[] args) throws FileNotFoundException {
-
-        FileInputStream fis = new FileInputStream(args[0]);
-
+    public static void main(String []args) throws FileNotFoundException {
+        try {
+            FileInputStream fis = new FileInputStream(args[0]);
+        } catch (ArrayIndexOutOfBoundsException aioobe) {
+            System.out.println("Error: No arguments passed in the commandline!");
+            System.out.println("Pass the name of the file to open as commandline argument");
+        }
     }
 }
