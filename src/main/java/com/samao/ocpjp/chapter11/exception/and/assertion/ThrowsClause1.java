07c045ef6d79a303492aecf29b4005941456dd23
hsamao <hsamao86@gmail.com>
2015-11-09 11:56:00 -0500

    listing 11.19 ==> The Throws Clause

---
+++ src/main/java/com/samao/ocpjp/chapter11/exception/and/assertion/ThrowsClause1.java
@@ -0,0 +1,16 @@
+package com.samao.ocpjp.chapter11.exception.and.assertion;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * Created by hsamao on 11/9/15.
+ */
+public class ThrowsClause1 {
+
+    public static void main (String [] args){
+        System.out.println("Reading an integer from the file 'integer.txt': ");
+        Scanner consoleScanner = new Scanner(new File("integer.txt"));
+        System.out.println("You typed the integer value: " + consoleScanner.nextInt());
+    }
+}
