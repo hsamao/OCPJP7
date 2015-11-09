f8a75ccdecdd4843a45f7bf6b868de26e3e1499a
hsamao <hsamao86@gmail.com>
2015-11-09 13:29:30 -0500

    listing 11.25 ==> Assertion

---
+++ src/main/java/com/samao/ocpjp/chapter11/exception/and/assertion/AssertionExample1.java
@@ -0,0 +1,18 @@
+package com.samao.ocpjp.chapter11.exception.and.assertion;
+
+/**
+ * Created by hsamao on 11/9/15.
+ */
+public class AssertionExample1 {
+        public static void main(String []args) {
+            int i = -10;
+            if(i < 0) {
+// if negative value, convert into positive value
+                i = -i;
+            }
+            System.out.println("the value of i is: " + i);
+// at this point the assumption is that i cannot be negative;
+// assert this condition since its an assumption that will always hold
+            assert (i >= 0) : "impossible: i is negative!";
+        }
+}
