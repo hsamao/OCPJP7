c51d3bc366144df69ed44a57f6cb6a742af2ddd5
hsamao <hsamao86@gmail.com>
2015-11-08 21:59:43 -0500

    listing 11.13 ==> Method Throws Exception

--- src/main/java/com/samao/ocpjp/chapter11/exception/and/assertion/CheckedExceptionExample1.java
+++ src/main/java/com/samao/ocpjp/chapter11/exception/and/assertion/CheckedExceptionExample1.java
@@ -1,13 +1,14 @@
 package com.samao.ocpjp.chapter11.exception.and.assertion;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * Created by hsamao on 11/8/15.
  */
 public class CheckedExceptionExample1 {
 
-    public static void main(String []args) {
+    public static void main(String []args) throws FileNotFoundException {
         FileInputStream fis = new FileInputStream(args[0]);
     }
 }
