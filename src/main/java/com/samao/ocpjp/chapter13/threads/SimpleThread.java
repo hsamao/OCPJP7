713a58d985b99d268beb6fc491d7142623100a76
hsamao <hsamao86@gmail.com>
2015-11-11 13:55:46 -0500

    listing 13.4 ==> Thread Name, Priority and Group

---
+++ src/main/java/com/samao/ocpjp/chapter13/threads/SimpleThread.java
@@ -0,0 +1,13 @@
+package com.samao.ocpjp.chapter13.threads;
+
+/**
+ * Created by hsamao on 11/11/15.
+ */
+public class SimpleThread {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+
+        System.out.println(thread);
+    }
+}
