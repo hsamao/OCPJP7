1836a5c43bf94178a104aed16777103aafe57627
hsamao <hsamao86@gmail.com>
2015-11-11 14:02:53 -0500

    listing 13.5 ==> Thread Priority

---
+++ src/main/java/com/samao/ocpjp/chapter13/threads/ThreadPriority.java
@@ -0,0 +1,13 @@
+package com.samao.ocpjp.chapter13.threads;
+
+/**
+ * Created by hsamao on 11/11/15.
+ */
+public class ThreadPriority {
+
+    public static void main(String[] args) {
+        System.out.println("Minimum priority of a thread is " + Thread.MIN_PRIORITY);
+        System.out.println("Normal priority of a thread is " + Thread.NORM_PRIORITY);
+        System.out.println("Maximum priority of a thread is " + Thread.MAX_PRIORITY);
+    }
+}
