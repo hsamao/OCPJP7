59d66a4b7209e33bfbd2ed3915bccefc349fbda0
hsamao <hsamao86@gmail.com>
2015-11-12 13:51:47 -0500

    listing 13.20 ==> Illegal Thread State Exception got fixed

--- src/main/java/com/samao/ocpjp/chapter13/threads/ThreadStateProblem.java
+++ src/main/java/com/samao/ocpjp/chapter13/threads/ThreadStateProblem.java
@@ -3,11 +3,19 @@ package com.samao.ocpjp.chapter13.threads;
 /**
  * Created by hsamao on 11/12/15.
  */
-public class ThreadStateProblem {
+public class ThreadStateProblem extends Thread {
+
+    synchronized public void run(){
+
+        try {
+            wait(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main (String [] args){
-        Thread thread = new Thread();
-        thread.start();
-        thread.start();
+
+        new ThreadStateProblem().start();
     }
 }
