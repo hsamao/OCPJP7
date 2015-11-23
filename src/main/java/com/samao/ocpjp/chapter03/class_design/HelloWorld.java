1d71a472b6d54b1da00eb9d36a74815e40a3dea3
hsamao <hsamao86@gmail.com>
2015-11-23 01:39:54 -0500

    switching branch without committing changes

---
+++ src/main/java/com/samao/ocpjp/chapter03/class_design/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.samao.ocpjp.chapter03.class_design;
+
+/**
+ * Created by hsamao on 11/23/15.
+ */
+public class HelloWorld {
+
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+}
